6052565abac935983381f872a74ed9c37613ee9e
Hien <thehienpv@gmail.com>
2024-08-25 10:19:50 +0700

    Correct build script

--- build_prod.sh
+++ build_prod.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Navigate to the container package and run the build command in the background
-(cd packages/container && npm run build) &
+(cd packages/container && npm install && npm run build) &
 
 # Navigate to the marketing package and run the build command in the background
-(cd packages/marketing && npm run build) &
+(cd packages/marketing && npm install && npm run build) &
 
 # Wait for all background processes to finish
 wait
