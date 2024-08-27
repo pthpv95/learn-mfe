5232c013fb01d660f174b9bbdff854ccde854d8b
Hien <thehienpv@gmail.com>
2024-08-27 13:42:58 +0700

    Update env

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,11 +1,21 @@
 import { mount } from "marketing/MarketingApp";
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default () => {
   let ref = React.useRef(null);
+  let history = useHistory();
 
   React.useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: ({ pathname: nextPath }) => {
+        if (nextPath && nextPath !== history.location.pathname) {
+          history.push(nextPath);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref} />;
